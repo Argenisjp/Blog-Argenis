16564cc79dcc83a8fe48710526f7a1f535418a3e
Argenisjp <73542063+Argenisjp@users.noreply.github.com>
2021-03-30 18:07:11 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("icon").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+	document.getElementById("move-content").classList.toggle('move-container-all');
+	document.getElementById("menu-responsive").classList.toggle('menu-lateral');
+}
+
+
+
+
+
